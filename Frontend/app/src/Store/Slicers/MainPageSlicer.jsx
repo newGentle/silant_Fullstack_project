3adf4d83b88dec397332ed2bf8fbd0b3c2ee87fc
newGentle <abdullaev.akbar@gmail.com>
2023-08-25 17:42:13 +0500

    .

--- Frontend/app/src/Store/Slicers/MainPageSlicer.jsx
+++ Frontend/app/src/Store/Slicers/MainPageSlicer.jsx
@@ -14,7 +14,7 @@ export const MachineData = createAsyncThunk(
     async (_, { rejectWithValue }) => {
         try {
             const { data } = await axios.get(
-                'http://127.0.0.1:8000/api/v1/machine/',
+                'http://127.0.0.1:8000/api/v1/order/',
                 
                 header
             );
